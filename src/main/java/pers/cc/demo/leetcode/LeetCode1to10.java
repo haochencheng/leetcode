a16de067241d856d4a543764ff0b022a3816f0eb
haochencheng <532224524@qq.com>
2018-06-07 18:34:54 +0800

    myAtoi

--- src/main/java/pers/cc/demo/leetcode/LeetCode1to10.java
+++ src/main/java/pers/cc/demo/leetcode/LeetCode1to10.java
@@ -1,5 +1,6 @@
 package pers.cc.demo.leetcode;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.concurrent.TransferQueue;
@@ -20,7 +21,6 @@ public class LeetCode1to10 {
     public void test() {
         System.out.println(Integer.parseInt("9999999991"));
     }
-
 
 
     /**
@@ -204,17 +204,17 @@ public class LeetCode1to10 {
 
     @Test
     public void lengthOfLongestSubstring() {
-       // System.out.println( LeetCode1to10.lengthOfLongestSubstring("abcabcbb"));
+        // System.out.println( LeetCode1to10.lengthOfLongestSubstring("abcabcbb"));
         //System.out.println( LeetCode1to10.lengthOfLongestSubstring("aab"));
         //System.out.println( LeetCode1to10.lengthOfLongestSubstring("bbb"));
-        System.out.println( LeetCode1to10.lengthOfLongestSubstring("xibkhhbsddxpmaosdkdswbkosweecxcbielrnojqsgh"));
+        System.out.println(LeetCode1to10.lengthOfLongestSubstring("xibkhhbsddxpmaosdkdswbkosweecxcbielrnojqsgh"));
     }
 
     @Test
     public void lengthOfLongestSubstring2() {
 //        System.out.println( LeetCode1to10.lengthOfLongestSubstring2("abcabcbb"));
 //          System.out.println( LeetCode1to10.lengthOfLongestSubstring2("aab"));
-        System.out.println( LeetCode1to10.lengthOfLongestSubstring2("biidygcc"));
+        System.out.println(LeetCode1to10.lengthOfLongestSubstring2("biidygcc"));
 
 //        System.out.println( LeetCode1to10.lengthOfLongestSubstring2("bbb"));
 //        System.out.println( LeetCode1to10.lengthOfLongestSubstring2("pwwkew"));
@@ -222,65 +222,64 @@ public class LeetCode1to10 {
     }
 
 
-
     @Test
     public void isStrRepeat() {
-        System.out.println( LeetCode1to10.isStrRepeat("abc"));
+        System.out.println(LeetCode1to10.isStrRepeat("abc"));
     }
 
     /*
-    * Given a string, find the length of the longest substring without repeating characters.
-    * Examples:
-    * Given "abcabcbb", the answer is "abc", which the length is 3.
-    *  Given "bbbbb", the answer is "b", with the length of 1.
-    *  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-    *
-    */
+     * Given a string, find the length of the longest substring without repeating characters.
+     * Examples:
+     * Given "abcabcbb", the answer is "abc", which the length is 3.
+     *  Given "bbbbb", the answer is "b", with the length of 1.
+     *  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+     *
+     */
     public static int lengthOfLongestSubstring(String s) {
-        String[] strs=s.split("");
-        LinkedList<String> linkedList=new LinkedList();
-        LinkedList<String> linkedListTemp=new LinkedList<String>();
-        for (String str:strs){
+        String[] strs = s.split("");
+        LinkedList<String> linkedList = new LinkedList();
+        LinkedList<String> linkedListTemp = new LinkedList<String>();
+        for (String str : strs) {
             linkedList.add(str);
             linkedListTemp.push(str);
         }
-        String tempSub="";
-        String strTemp="";
-        while (linkedList.size()!=0){
-                strTemp=linkedList.poll();
-                for (int i = 0; i < linkedList.size(); i++) {
-                    if (i==0){
-                        tempSub=strTemp+linkedList.get(i);
-                    }else {
-                        tempSub+=linkedList.get(i);
-                    }
-                    if (!isStrRepeat(tempSub)){
-                        linkedListTemp.push(tempSub);
-                    }
+        String tempSub = "";
+        String strTemp = "";
+        while (linkedList.size() != 0) {
+            strTemp = linkedList.poll();
+            for (int i = 0; i < linkedList.size(); i++) {
+                if (i == 0) {
+                    tempSub = strTemp + linkedList.get(i);
+                } else {
+                    tempSub += linkedList.get(i);
                 }
+                if (!isStrRepeat(tempSub)) {
+                    linkedListTemp.push(tempSub);
+                }
+            }
         }
         final String[] tempStr = {""};
-        linkedListTemp.forEach((c)->{
-            if (c.length()> tempStr[0].length()){
-                tempStr[0] =c;
+        linkedListTemp.forEach((c) -> {
+            if (c.length() > tempStr[0].length()) {
+                tempStr[0] = c;
             }
-       });
+        });
         return tempStr[0].length();
     }
 
     public static int lengthOfLongestSubstring2(String s) {
-        String[] strings=s.split("");
-        LinkedList<String> linkedList=new LinkedList<>();
-        if (!isStrRepeat(s)){
+        String[] strings = s.split("");
+        LinkedList<String> linkedList = new LinkedList<>();
+        if (!isStrRepeat(s)) {
             return s.length();
         }
         return 0;
     }
 
-    public static boolean isStrRepeat(LinkedList<String> linkedList){
+    public static boolean isStrRepeat(LinkedList<String> linkedList) {
         for (int i = 0; i < linkedList.size(); i++) {
-            String str=linkedList.get(i);
-            if (linkedList.indexOf(str)!=linkedList.lastIndexOf(str)){
+            String str = linkedList.get(i);
+            if (linkedList.indexOf(str) != linkedList.lastIndexOf(str)) {
                 return true;
             }
         }
@@ -288,11 +287,10 @@ public class LeetCode1to10 {
     }
 
 
-
-    public static boolean isStrRepeat(String str){
-        String[] strs=str.split("");
-        for (String s:strs){
-            if (str.lastIndexOf(s)!=str.indexOf(s)){
+    public static boolean isStrRepeat(String str) {
+        String[] strs = str.split("");
+        for (String s : strs) {
+            if (str.lastIndexOf(s) != str.indexOf(s)) {
                 return true;
             }
         }
@@ -391,47 +389,202 @@ public class LeetCode1to10 {
 
     /**
      * 7. 反转整数
+     * <p>
+     * 给定一个 32 位有符号整数，将整数中的数字进行反转。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: 123
+     * 输出: 321
+     * 示例 2:
+     * <p>
+     * 输入: -123
+     * 输出: -321
+     * 示例 3:
+     * <p>
+     * 输入: 120
+     * 输出: 21
+     * 注意:
+     * <p>
+     * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
      *
-     给定一个 32 位有符号整数，将整数中的数字进行反转。
-
-     示例 1:
-
-     输入: 123
-     输出: 321
-     示例 2:
-
-     输入: -123
-     输出: -321
-     示例 3:
-
-     输入: 120
-     输出: 21
-     注意:
-
-     假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
      * @param x
      * @return
      */
-    private int swap(int x){
+    private int swap(int x) {
         try {
-            boolean min=false;
-            if (x<0){
-                x=-x;
-            }else {
-                min=true;
+            boolean min = false;
+            if (x < 0) {
+                x = -x;
+            } else {
+                min = true;
             }
-            String[] strings=Integer.valueOf(x).toString().split("");
-            StringBuilder stringBuilder=new StringBuilder();
-            for (int j=strings.length-1;j>=0;j--){
+            String[] strings = Integer.valueOf(x).toString().split("");
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = strings.length - 1; j >= 0; j--) {
                 stringBuilder.append(strings[j]);
             }
-            String str=stringBuilder.toString();
-            return min?Integer.valueOf(str.toString()):-Integer.valueOf(str.toString());
-        }catch (Exception e){
+            String result = stringBuilder.toString();
+            return min ? Integer.valueOf(result.toString()) : -Integer.valueOf(result.toString());
+        } catch (Exception e) {
             return 0;
         }
     }
 
+    /**
+     * 8. 字符串转整数 (atoi)
+     * 实现 atoi，将字符串转为整数。
+     * <p>
+     * 在找到第一个非空字符之前，需要移除掉字符串中的空格字符。如果第一个非空字符是正号或负号，选取该符号，并将其与后面尽可能多的连续的数字组合起来，这部分字符即为整数的值。如果第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     * <p>
+     * 字符串可以在形成整数的字符后面包括多余的字符，这些字符可以被忽略，它们对于函数没有影响。
+     * <p>
+     * 当字符串中的第一个非空字符序列不是个有效的整数；或字符串为空；或字符串仅包含空白字符时，则不进行转换。
+     * <p>
+     * 若函数不能执行有效的转换，返回 0。
+     * <p>
+     * 说明：
+     * <p>
+     * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。如果数值超过可表示的范围，则返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: "42"
+     * 输出: 42
+     * 示例 2:
+     * <p>
+     * 输入: "   -42"
+     * 输出: -42
+     * 解释: 第一个非空白字符为 '-', 它是一个负号。
+     * 我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。
+     * 示例 3:
+     * <p>
+     * 输入: "4193 with words"
+     * 输出: 4193
+     * 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
+     * 示例 4:
+     * <p>
+     * 输入: "words and 987"
+     * 输出: 0
+     * 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。
+     * 因此无法执行有效的转换。
+     * 示例 5:
+     * <p>
+     * 输入: "-91283472332"
+     * 输出: -2147483648
+     * 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。
+     * 因此返回 INT_MIN (−231) 。
+     *
+     * @param str
+     * @return
+     */
+    public static int myAtoi(String str) {
+        char[] value = str.toCharArray();
+        int len = value.length;
+        char[] val = value;    /* avoid getfield opcode */
+        int begin = -1;
+        int end = -1;
+        String symbol=null;
+        boolean next=true;
+        if (len==1){
+                return str.matches("\\d+")?Integer.valueOf(str):0;
+        }
+        for (int i = 0; i < len - 1; i++) {
+            if (String.valueOf(val[i]).matches("\\d+|\\+|-")) {
+                if (String.valueOf(val[i]).matches("\\+|-")){
+                    symbol=String.valueOf(val[i]);
+                    if (String.valueOf(val[i + 1]).matches("\\d+")&& val[i + 1] != '0') {
+                        if (begin==-1){
+                            begin = i;
+                            end = i + 1;
+                        }
+
+                    } else {
+                        if (begin==-1) {
+                            end = i;
+                            next = false;
+                        }
+                    }
+                }else {
+                    if (String.valueOf(val[i]).matches("\\d+")&& val[i + 1] != '0'){
+                        if (begin==-1){
+                            begin = i;
+                        }
+                        if (next&&begin!=-1){
+                            end = i + 1;
+                        }
+                    }
+                    if (String.valueOf(val[i + 1]).matches("\\d+")) {
+                        if (next&&begin!=-1){
+                            end = i + 1;
+                        }
+                    } else {
+
+                        if (next&&begin!=-1){
+                            end = i;
+                        }
+                        next=false;
+                    }
+                }
+            }else {
+                if (val[i] == ' '){
+                    continue;
+                }else {
+                    break;
+                }
+            }
+        }
+        if (end == -1) {
+            return 0;
+        }
+        String result =str.subSequence(begin, end + 1).toString().replaceAll(" ", "");
+        if (symbol!=null){
+            if (result.length()==1){
+                return 0;
+            }
+            if (result.length()>String.valueOf(Integer.MIN_VALUE).length()){
+                return Integer.MIN_VALUE;
+            }else if (result.length()==String.valueOf(Integer.MIN_VALUE).length()){
+                if (result.compareTo(String.valueOf(Integer.MIN_VALUE))>0){
+                    return Integer.MIN_VALUE;
+                }
+            }
+            return Integer.valueOf(result.toString());
+        }
+        if (result.length()>String.valueOf(Integer.MAX_VALUE).length()){
+            return Integer.MAX_VALUE;
+        }else if (result.length()==String.valueOf(Integer.MAX_VALUE).length()){
+            if (result.compareTo(String.valueOf(Integer.MAX_VALUE))>0){
+                return Integer.MAX_VALUE;
+            }
+        }
+        return Integer.valueOf(result.toString());
+
+    }
+
+    @Test
+    public void myAtoiTest() {
+//        String str =  "42";
+//        String str =  "   -42";
+//        String str =  "-91283472332";
+//        String str = "2147483648";
+//        String str = "-2147483648";
+//        String str = "3.14159";
+//        String str = "+-2";
+//        String str = "1";
+//        String str ="  0000000000012345678";
+//        String str = "1a";
+//          String str = "   +0 123";
+//        String str = "20000000000000000000";
+//     String str = "-3147483648";
+//     String str = "-   234";
+//     String str = "0-1";
+//     String str ="4193 with words";
+//     String str ="words and 987";
+//     String str ="000000000000000000";
+     String str ="-000000000000001";
+        System.out.println(myAtoi(str));
+    }
 
 
 }
