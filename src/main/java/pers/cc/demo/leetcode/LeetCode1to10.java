6902b3e335616e79e8a188985964573a872566f9
haochencheng <532224524@qq.com>
2018-06-07 15:36:27 +0800

    07

--- src/main/java/pers/cc/demo/leetcode/LeetCode1to10.java
+++ src/main/java/pers/cc/demo/leetcode/LeetCode1to10.java
@@ -389,4 +389,49 @@ public class LeetCode1to10 {
         return true;
     }
 
+    /**
+     * 7. 反转整数
+     *
+     给定一个 32 位有符号整数，将整数中的数字进行反转。
+
+     示例 1:
+
+     输入: 123
+     输出: 321
+     示例 2:
+
+     输入: -123
+     输出: -321
+     示例 3:
+
+     输入: 120
+     输出: 21
+     注意:
+
+     假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
+     * @param x
+     * @return
+     */
+    private int swap(int x){
+        try {
+            boolean min=false;
+            if (x<0){
+                x=-x;
+            }else {
+                min=true;
+            }
+            String[] strings=Integer.valueOf(x).toString().split("");
+            StringBuilder stringBuilder=new StringBuilder();
+            for (int j=strings.length-1;j>=0;j--){
+                stringBuilder.append(strings[j]);
+            }
+            String str=stringBuilder.toString();
+            return min?Integer.valueOf(str.toString()):-Integer.valueOf(str.toString());
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+
+
 }
