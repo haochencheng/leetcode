302bb4e348331186697f7e7470f412770c0e2059
haochencheng <532224524@qq.com>
2020-01-21 16:10:49 +0800

    74. 搜索二维矩阵

---
+++ src/main/java/pers/cc/demo/leetcode/array/SearchMatrix.java
@@ -0,0 +1,75 @@
+package pers.cc.demo.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 74. 搜索二维矩阵
+ * <p>
+ * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
+ * <p>
+ * 每行中的整数从左到右按升序排列。
+ * 每行的第一个整数大于前一行的最后一个整数。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * matrix = [
+ * [1,   3,  5,  7],
+ * [10, 11, 16, 20],
+ * [23, 30, 34, 50]
+ * ]
+ * target = 3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * matrix = [
+ * [1,   3,  5,  7],
+ * [10, 11, 16, 20],
+ * [23, 30, 34, 50]
+ * ]
+ * target = 13
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @description:
+ * @author: haochencheng
+ * @create: 2020-01-21 16:00
+ **/
+public class SearchMatrix {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        SearchMatrix searchMatrix = new SearchMatrix();
+        boolean result = searchMatrix.searchMatrix(matrix, 3);
+        System.out.println(result);
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        if (m==0||n==0){
+            return false;
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0]>target){
+                return false;
+            }else if (matrix[i][n-1]<target){
+                continue;
+            }
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+}
